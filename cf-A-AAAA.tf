f349c1e64b8b0711572f0c6daf3b9dfa97c30114
denis.itpro <denis.itpro@gmail.com>
2024-03-28 11:29:09 +0300

    remove server hat

--- cf-A-AAAA.tf
+++ cf-A-AAAA.tf
@@ -38,23 +38,23 @@ resource "cloudflare_record" "k8s_worker_ipv6" {
   allow_overwrite = true
 }
 
-
-resource "cloudflare_record" "hat_ipv4" {
-  zone_id         = local.current_cf_zone_id
-  count           = length(hcloud_server.hat_app_v1)
-  name            = hcloud_server.hat_app_v1[count.index].name
-  value           = hcloud_server.hat_app_v1[count.index].ipv4_address
-  type            = "A"
-  proxied         = false
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "hat_ipv6" {
-  zone_id         = local.current_cf_zone_id
-  count           = length(hcloud_server.hat_app_v1)
-  name            = hcloud_server.hat_app_v1[count.index].name
-  value           = hcloud_server.hat_app_v1[count.index].ipv6_address
-  type            = "AAAA"
-  proxied         = false
-  allow_overwrite = true
-}
+#
+#resource "cloudflare_record" "hat_ipv4" {
+#  zone_id         = local.current_cf_zone_id
+#  count           = length(hcloud_server.hat_app_v1)
+#  name            = hcloud_server.hat_app_v1[count.index].name
+#  value           = hcloud_server.hat_app_v1[count.index].ipv4_address
+#  type            = "A"
+#  proxied         = false
+#  allow_overwrite = true
+#}
+#
+#resource "cloudflare_record" "hat_ipv6" {
+#  zone_id         = local.current_cf_zone_id
+#  count           = length(hcloud_server.hat_app_v1)
+#  name            = hcloud_server.hat_app_v1[count.index].name
+#  value           = hcloud_server.hat_app_v1[count.index].ipv6_address
+#  type            = "AAAA"
+#  proxied         = false
+#  allow_overwrite = true
+#}
--- srv-hat-stats.tf
+++ srv-hat-stats.tf
@@ -1,22 +1,22 @@
-resource "hcloud_server" "hat_app_v1" {
-  count       = 1
-  name        = "hat-${format("%02d", count.index + 1)}"
-  server_type = "cx21"
-  image       = "ubuntu-22.04"
-  location    = "fsn1"
-  ssh_keys = [
-    hcloud_ssh_key.den_ssh_key.id
-  ]
-  user_data = file("./user_data/user_data_hetzner_v1.sh")
-
-  lifecycle {
-    ignore_changes = [user_data, image]
-  }
-
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-
-}
-
+#resource "hcloud_server" "hat_app_v1" {
+#  count       = 1
+#  name        = "hat-${format("%02d", count.index + 1)}"
+#  server_type = "cx21"
+#  image       = "ubuntu-22.04"
+#  location    = "fsn1"
+#  ssh_keys = [
+#    hcloud_ssh_key.den_ssh_key.id
+#  ]
+#  user_data = file("./user_data/user_data_hetzner_v1.sh")
+#
+#  lifecycle {
+#    ignore_changes = [user_data, image]
+#  }
+#
+#  public_net {
+#    ipv4_enabled = true
+#    ipv6_enabled = true
+#  }
+#
+#}
+#
