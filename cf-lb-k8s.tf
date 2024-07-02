4a26be627b502341af2247ea0f115affa7db7f2a
denis.itpro <denis.itpro@gmail.com>
2024-07-02 14:42:06 +0300

    support ubuntu 24 and worker node k8s

--- cf-lb-k8s.tf
+++ cf-lb-k8s.tf
@@ -17,20 +17,20 @@ resource "cloudflare_record" "k8s_master_lb_ipv6" {
 }
 
 
-# resource "cloudflare_record" "k8s_worker_lb_ipv4" {
-#   zone_id         = local.current_cf_zone_id
-#   name            = "worker"
-#   value           = hcloud_load_balancer.k8s_worker_lb.ipv4
-#   type            = "A"
-#   proxied         = false
-#   allow_overwrite = true
-# }
-#
-# resource "cloudflare_record" "k8s_worker_lb_ipv6" {
-#   zone_id         = local.current_cf_zone_id
-#   name            = "worker"
-#   value           = hcloud_load_balancer.k8s_worker_lb.ipv6
-#   type            = "AAAA"
-#   proxied         = false
-#   allow_overwrite = true
-# }
+resource "cloudflare_record" "k8s_worker_lb_ipv4" {
+  zone_id         = local.current_cf_zone_id
+  name            = "worker"
+  value           = hcloud_load_balancer.k8s_worker_lb.ipv4
+  type            = "A"
+  proxied         = false
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "k8s_worker_lb_ipv6" {
+  zone_id         = local.current_cf_zone_id
+  name            = "worker"
+  value           = hcloud_load_balancer.k8s_worker_lb.ipv6
+  type            = "AAAA"
+  proxied         = false
+  allow_overwrite = true
+}
