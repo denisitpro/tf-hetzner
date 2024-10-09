45202414a206c1319cca88b9e683e4adf52ca673
denis.itpro <denis.itpro@gmail.com>
2024-10-09 09:54:39 +0300

    formatted

--- cf-A-AAAA.tf
+++ cf-A-AAAA.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "k8s_master_ipv4" {
   zone_id         = local.current_cf_zone_id
   count           = length(hcloud_server.k8s_master)
   name            = hcloud_server.k8s_master[count.index].name
-  content           = hcloud_server.k8s_master[count.index].ipv4_address
+  content         = hcloud_server.k8s_master[count.index].ipv4_address
   type            = "A"
   ttl             = 300
   proxied         = false
@@ -14,7 +14,7 @@ resource "cloudflare_record" "k8s_master_ipv6" {
   zone_id         = local.current_cf_zone_id
   count           = length(hcloud_server.k8s_master)
   name            = hcloud_server.k8s_master[count.index].name
-  content           = hcloud_server.k8s_master[count.index].ipv6_address
+  content         = hcloud_server.k8s_master[count.index].ipv6_address
   type            = "AAAA"
   ttl             = 300
   proxied         = false
@@ -25,7 +25,7 @@ resource "cloudflare_record" "k8s_worker_ipv4" {
   zone_id         = local.current_cf_zone_id
   count           = length(hcloud_server.k8s_worker)
   name            = hcloud_server.k8s_worker[count.index].name
-  content           = hcloud_server.k8s_worker[count.index].ipv4_address
+  content         = hcloud_server.k8s_worker[count.index].ipv4_address
   type            = "A"
   ttl             = 300
   proxied         = false
@@ -36,7 +36,7 @@ resource "cloudflare_record" "k8s_worker_ipv6" {
   zone_id         = local.current_cf_zone_id
   count           = length(hcloud_server.k8s_worker)
   name            = hcloud_server.k8s_worker[count.index].name
-  content           = hcloud_server.k8s_worker[count.index].ipv6_address
+  content         = hcloud_server.k8s_worker[count.index].ipv6_address
   type            = "AAAA"
   ttl             = 300
   proxied         = false
--- cf-lb-k8s.tf
+++ cf-lb-k8s.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "k8s_master_lb_ipv4" {
   zone_id         = local.current_cf_zone_id
   name            = "k8s"
-  content           = hcloud_load_balancer.k8s_master_lb.ipv4
+  content         = hcloud_load_balancer.k8s_master_lb.ipv4
   type            = "A"
   ttl             = 300
   proxied         = false
@@ -11,7 +11,7 @@ resource "cloudflare_record" "k8s_master_lb_ipv4" {
 resource "cloudflare_record" "k8s_master_lb_ipv6" {
   zone_id         = local.current_cf_zone_id
   name            = "k8s"
-  content           = hcloud_load_balancer.k8s_master_lb.ipv6
+  content         = hcloud_load_balancer.k8s_master_lb.ipv6
   type            = "AAAA"
   ttl             = 300
   proxied         = false
