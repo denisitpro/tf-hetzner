5b27a0f7bea8b708cd829c6fad476e4098f9c477
denis.itpro <denis.itpro@gmail.com>
2024-11-11 17:46:44 +0300

    test cluster k8s support internal address

--- cf-A-AAAA.tf
+++ cf-A-AAAA.tf
@@ -30,30 +30,28 @@ resource "cloudflare_record" "k8s_master_ipv6" {
   proxied         = false
   allow_overwrite = true
 }
-#
-# resource "cloudflare_record" "k8s_worker_ipv4" {
-#   zone_id         = local.current_cf_zone_id
-#   count           = length(hcloud_server.k8s_worker)
-#   name            = hcloud_server.k8s_worker[count.index].name
-#   content         = hcloud_server.k8s_worker[count.index].ipv4_address
-#   type            = "A"
-#   ttl             = 300
-#   proxied         = false
-#   allow_overwrite = true
-# }
-#
-# resource "cloudflare_record" "k8s_worker_ipv6" {
-#   zone_id         = local.current_cf_zone_id
-#   count           = length(hcloud_server.k8s_worker)
-#   name            = hcloud_server.k8s_worker[count.index].name
-#   content         = hcloud_server.k8s_worker[count.index].ipv6_address
-#   type            = "AAAA"
-#   ttl             = 300
-#   proxied         = false
-#   allow_overwrite = true
-# }
 
+resource "cloudflare_record" "k8s_worker_ipv4" {
+  zone_id         = local.current_cf_zone_id
+  count           = length(hcloud_server.k8s_worker)
+  name            = hcloud_server.k8s_worker[count.index].name
+  content         = hcloud_server.k8s_worker[count.index].ipv4_address
+  type            = "A"
+  ttl             = 300
+  proxied         = false
+  allow_overwrite = true
+}
 
+resource "cloudflare_record" "k8s_worker_ipv6" {
+  zone_id         = local.current_cf_zone_id
+  count           = length(hcloud_server.k8s_worker)
+  name            = hcloud_server.k8s_worker[count.index].name
+  content         = hcloud_server.k8s_worker[count.index].ipv6_address
+  type            = "AAAA"
+  ttl             = 300
+  proxied         = false
+  allow_overwrite = true
+}
 
 resource "cloudflare_record" "haproxy_lb_ipv4" {
   zone_id         = local.current_cf_zone_id
@@ -77,8 +75,6 @@ resource "cloudflare_record" "haproxy_lb_local_ipv4" {
   proxied         = false
   allow_overwrite = true
 }
-
-
 
 resource "cloudflare_record" "haproxy_lb_ipv6" {
   zone_id         = local.current_cf_zone_id
