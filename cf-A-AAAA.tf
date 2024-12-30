38f7c8ea81ae83483680217c144d1d56c25024c0
denis.itpro <denis.itpro@gmail.com>
2024-12-30 17:50:10 +0300

    swfs volume add

--- cf-A-AAAA.tf
+++ cf-A-AAAA.tf
@@ -124,11 +124,34 @@
 #   allow_overwrite = true
 # }
 
-resource "cloudflare_record" "seaweedfs_ipv6" {
+
+resource "cloudflare_record" "swfs_master_ip4" {
   zone_id         = local.current_cf_zone_id
-  count           = length(hcloud_server.seaweed_master)
-  name            = hcloud_server.seaweed_master[count.index].name
-  content         = hcloud_server.seaweed_master[count.index].ipv6_address
+  count = length(hcloud_server.swfs_master)
+  name            = hcloud_server.swfs_master[count.index].name
+  content         = hcloud_server.swfs_master[count.index].ipv4_address
+  type            = "A"
+  ttl             = 300
+  proxied         = false
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "swfs_master_ipv6" {
+  zone_id         = local.current_cf_zone_id
+  count           = length(hcloud_server.swfs_master)
+  name            = hcloud_server.swfs_master[count.index].name
+  content         = hcloud_server.swfs_master[count.index].ipv6_address
+  type            = "AAAA"
+  ttl             = 300
+  proxied         = false
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "swfs_volume_ipv6" {
+  zone_id         = local.current_cf_zone_id
+  count           = length(hcloud_server.swfs_volume)
+  name            = hcloud_server.swfs_volume[count.index].name
+  content         = hcloud_server.swfs_volume[count.index].ipv6_address
   type            = "AAAA"
   ttl             = 300
   proxied         = false
